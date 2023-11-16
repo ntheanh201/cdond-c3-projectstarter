85dc9f9a2ff37a1d59cebb0361e4dbe1ef72ff4a
The Anh Nguyen <ntheanh201@gmail.com>
2023-11-16 16:53:51 +0100

    update

--- backend/src/modules/config/config.service.ts
+++ backend/src/modules/config/config.service.ts
@@ -108,9 +108,6 @@ export class ConfigService {
       logging: this.envConfig.TYPEORM_LOGGING === 'true',
       extra: { max: 4, min: 1 },
       synchronize: false,
-      ssl: {
-        rejectUnauthorized: false,
-      },
     };
   }
 
